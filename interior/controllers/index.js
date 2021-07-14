38ce12bb418831b05e3961af790fd9f602c6086c
hilmanauz <hilmanauz@gmail.com>
2021-07-14 22:47:26 +0700

    feature: add filter and search API

--- interior/controllers/index.js
+++ interior/controllers/index.js
@@ -1,8 +1,20 @@
 const {Toko, Kategori, Motor} = require("../models");
+const _ = require('lodash')
+const { Op } = require("sequelize");
 
 class Controller {
-  static viewAllToko(req, res, next) {
-    Toko.findAll({include: [{model: Kategori}, {model: Motor}]})
+  static viewAllKategori(req, res, next) {
+    Kategori.findAll()
+      .then(data => {
+        res.status(200).json(data)
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
+  }
+
+  static viewAllMotor(req, res, next) {
+    Motor.findAll()
       .then(data => {
         res.status(200).json(data)
       })
@@ -29,9 +41,8 @@ class Controller {
       })
   }
 
-  static viewAllToko(req, res, next) {
-    let result = {}
-    Promise.all([Toko.findAll({include: [{model: Kategori}, {model: Motor}]}), Kategori.findAll(), Motor.findAll()])
+  static viewSearchAndPositionToko(req, res, next) {
+    Toko.findAll({attributes: ['nama', 'lat', 'long', 'mapUrl', 'id']})
       .then(data => {
         res.status(200).json(data)
       })
@@ -40,10 +51,77 @@ class Controller {
       })
   }
 
-  static filterToko(req, res, next) {
-    
+  static viewTokoPagination(req, res, next) {
+    Toko.findAll({include: [{model: Kategori}, {model: Motor}],  order: [['id', 'ASC']]})
+      .then(data => {
+        const pagination = _.chunk(data, 5)
+        const page = Number(req.params.page)
+        const dataLength = pagination.length
+        const result = {
+          data: pagination[page-1],
+          length: dataLength,
+          currentPage: Number(req.params.page),
+        }
+        res.status(200).json(result)
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
   }
 
+  static cariToko(req, res, next) {
+    Toko.findAll({
+      include: [{model: Kategori}, {model: Motor}],  
+      order: [['id', 'ASC']],
+      where: {
+      nama: {
+        [Op.iLike]: '%' + req.body.search + '%'
+      }
+    }})
+      .then(data => {
+        const pagination = _.chunk(data, 5)
+        const dataLength = pagination.length
+        const page = Number(req.params.page)
+        const result = {
+          data: pagination[page-1],
+          length: dataLength,
+          currentPage: Number(req.params.page),
+        }
+        res.status(200).json(result)
+      })
+      .catch(err => {
+        res.status(500).json(err)
+      })
+  }
+
+  static filterToko(req, res, next) {
+    // let myFunc = num => Number(num);
+    // var kategoriId = Array.from(String(req.body.KategoriId), myFunc);
+    // var motorId = Array.from(String(req.body.MotorId), myFunc);
+    Toko.findAll({include: [{model: Kategori}, {model: Motor}],
+      where: {
+      MotorId: {
+        [Op.or]: req.body.MotorId
+      },
+      KategoriId: {
+        [Op.or]: req.body.KategoriId
+      }
+    }})
+    .then(data => {
+      const pagination = _.chunk(data, 5)
+      const dataLength = pagination.length
+      const page = Number(req.params.page)
+      const result = {
+        data: pagination[page-1],
+        length: dataLength,
+        currentPage: Number(req.params.page),
+      }
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
+  }
 }
 
 module.exports = Controller
--- interior/routers/index.js
+++ interior/routers/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const Controller = require("../controllers");
 
-router.get('/toko', Controller.viewAllToko);
+router.get('/toko', Controller.viewSearchAndPositionToko);
+router.get('/toko/:page', Controller.viewTokoPagination);
+router.post('/cariToko/:page', Controller.cariToko);
+router.post('/filterToko/:page', Controller.filterToko);
+router.get('/motor', Controller.viewAllMotor);
+router.get('/kategori', Controller.viewAllKategori);
 router.post('/toko', Controller.createToko);
 
 module.exports = router
